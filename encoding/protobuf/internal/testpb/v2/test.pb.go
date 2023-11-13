e4a5382a6a030b0c94abeaa916d9c8b54c0b5768
rhang <rhang@uber.com>
2023-11-13 22:57:15 +0000

    pass dev: Update copyright date

--- encoding/protobuf/internal/testpb/v2/test.pb.go
+++ encoding/protobuf/internal/testpb/v2/test.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.5.1
 // source: encoding/protobuf/internal/testpb/v2/test.proto
 
-// Copyright (c) 2022 Uber Technologies, Inc.
+// Copyright (c) 2023 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
